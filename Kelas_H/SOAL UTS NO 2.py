d4243bfdaeaafd843a87504b6557173a33595da9
Ridha0510 <2403010199@unper.ac.id>
2024-11-11 17:28:33 +0700

    Ridha Ardiansyah Maulana Ramadhan-2403010199

---
+++ Kelas_H/SOAL UTS NO 2.py
@@ -0,0 +1,6 @@
+# Konversi Suhu Celcius ke Fahrenheit
+celcius = float(input("Masukkan suhu dalam Celcius: "))
+
+fahrenheit = (celcius * 9/5) + 32
+
+print(f"Suhu dalam Fahrenheit: {fahrenheit}")
---
+++ Kelas_H/SOAL UTS NO 3.py
@@ -0,0 +1,7 @@
+# Menggabungkan Dua Kata
+kata_pertama = input("Masukkan kata pertama: ")
+kata_kedua = input("Masukkan kata kedua: ")
+
+hasil_gabungan = kata_pertama + kata_kedua
+
+print(f"Hasil penggabungan: {hasil_gabungan}")
